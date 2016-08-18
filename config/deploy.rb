44af5e1dfe620472e6c5a9f595d89aaab8da49e3
floganz <alexander.0072@gmail.com>
2016-08-18 09:28:12 +0300

    paths fixs

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,11 +14,12 @@ set :stage,           :production
 set :deploy_via,      :remote_cache
 #set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
 set :deploy_to,       "/var/www/light-it-03.tk"
-set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
-set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
-set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
-set :puma_access_log, "#{release_path}/log/puma.error.log"
-set :puma_error_log,  "#{release_path}/log/puma.access.log"
+# set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+set :puma_bind,       "unix:///var/www/light-it-03.tk/shared/tmp/sockets/testers-app-puma.sock"
+set :puma_state,      "/var/www/light-it-03.tk/shared/tmp/pids/puma.state"
+set :puma_pid,        "/var/www/light-it-03.tk/shared/tmp/pids/puma.pid"
+set :puma_access_log, "/var/www/light-it-03.tk/shared/log/puma.error.log"
+set :puma_error_log,  "/var/www/light-it-03.tk/shared/log/puma.access.log"
 set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub),
                         password: '8round6z' }
 set :puma_preload_app, true
--- config/puma.rb
+++ config/puma.rb
@@ -12,14 +12,14 @@ rails_env = ENV['RAILS_ENV'] || "production"
 environment rails_env
 
 # Set up socket location
-bind "unix:///var/www/light-it-03.tk/shared/sockets/puma.sock"
+bind "unix:///var/www/light-it-03.tk/shared/tmp/sockets/testers-app-puma.sock"
 
 # Logging
 stdout_redirect "/var/www/light-it-03.tk/shared/log/puma.stdout.log", "/var/www/light-it-03.tk/shared/log/puma.stderr.log", true
 
 # Set master PID and state locations
-pidfile "/var/www/light-it-03.tk/sharedpids/puma.pid"
-state_path "/var/www/light-it-03.tk/shared/pids/puma.state"
+pidfile "/var/www/light-it-03.tk/shared/tmp/pids/puma.pid"
+state_path "/var/www/light-it-03.tk/shared/tmp/pids/puma.state"
 activate_control_app
 
 on_worker_boot do
